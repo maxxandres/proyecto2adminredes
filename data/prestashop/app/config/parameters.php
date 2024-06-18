c0711120ccea80900eb106361f7f658c17fe140d
maxxandres <mmaure19@alumnos.utalca.cl>
2024-06-18 10:08:32 -0400

    first commit

---
+++ data/prestashop/app/config/parameters.php
@@ -0,0 +1,64 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'db',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'password',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'Mad7hfNrE5SpI93pDL78OOIvzqMFNadiK47LEhi3d0njnTp03WTYgK3PEXS7Bi9p',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2024-06-18',
+    'locale' => 'es-ES',
+    'use_debug_toolbar' => true,
+    'cookie_key' => 'G6Fmrru0t1tJs7Cke8UgqFZ5U1dXg9uApZzswpLxb5xVpKpyKjZT8FRSfMFk3aSB',
+    'cookie_iv' => '1mAJhMr0nqPF06OIynmdg9SasFk8SoaS',
+    'new_cookie_key' => 'def000006fba0e9098c4b5c031f16d95de28dea11e3a978daccce40d8e3f3f19f27a24dad3638e1910ce8cd6717c99eaaf280fdffc9d0b4b5241b4917182cfaa12012377',
+    'api_public_key' => '-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6sQ28muGMsbXIwfAZctD
+//CMiU0vhvMlsXfMbnRrhlYZPQ07tgUizRRYGLiwvyPRRe10eUG1oHpNbtdGDrhB
+Chrsl2QAtWIB6ubOdJAaT8IEym7Tgzh9Kta05/IWqci5UhogzKxHEBfa3jkkkNv0
+Ir86Anf0m2vhYo/t29V4CX/kw2+TeSu20gjGqP9xHztceBxNzPuQ6LL7vAs+TrJw
+KysekY6S7EOlZq/1cNjyzR6Ts2vAVMqRnh5qLtvBfdUD18K2M3zLOM1ackogRzgm
+NotHeGctonqdL7XGiT2wdQCy0W/AtgvgeYt7hAOZLor09t0pnS1nqjUoz8eI4lXB
+9QIDAQAB
+-----END PUBLIC KEY-----
+',
+    'api_private_key' => '-----BEGIN PRIVATE KEY-----
+MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDqxDbya4Yyxtcj
+B8Bly0P/8IyJTS+G8yWxd8xudGuGVhk9DTu2BSLNFFgYuLC/I9FF7XR5QbWgek1u
+10YOuEEKGuyXZAC1YgHq5s50kBpPwgTKbtODOH0q1rTn8hapyLlSGiDMrEcQF9re
+OSSQ2/QivzoCd/Sba+Fij+3b1XgJf+TDb5N5K7bSCMao/3EfO1x4HE3M+5Dosvu8
+Cz5OsnArKx6RjpLsQ6Vmr/Vw2PLNHpOza8BUypGeHmou28F91QPXwrYzfMs4zVpy
+SiBHOCY2i0d4Zy2iep0vtcaJPbB1ALLRb8C2C+B5i3uEA5kuivT23SmdLWeqNSjP
+x4jiVcH1AgMBAAECggEAKC53PRy8NlqkHeHIw2N7+wVf+GwWk2FwdSSM/+a7NJ3t
+6e4m5yuP3kv6HBkYX06xFefjhJGDlbj5IdyEqCZPM3ySPAMov+LLczoX4gl8B2Ss
+zz0SxKXZfoiLJBviVyL83RmNvZpK+20pz9k18flDmuLh+eQwIJ3hUK5TxsmNpwpB
+1gXi4nf9QRMDeTxGJ40OPl92BCd+jeCxlfbxAwmI7dCYdkjAbk+q3+kgRoS2x3vH
+4bSitdk/gm7yxZvXttc7S5dnh5TrCqW1t/Mcn69yitXxK8gTrujbuShijpkLRe3V
+Hii2gjN7XWOyLwv9qWH+Lknqu3G7tyIMrU3hlMqbrQKBgQD9D2aVDbfb5os5XOfh
+3RiFxvhydltKAs91gmEh9ZyKmbVywr87TmG2D4BS6w002bTKlQxdMVr+dNkIehVr
+EIPYNBVYktVMtL8lfEU0BDUsop06Tq5jUdWAVV8FC85K6hrMLujKJxVUcX7mdLN3
+9o2ocLZvQnrX0QPx+ZfdjT1lCwKBgQDtfmiZceQxnwGRTu2C+9xeELRh//rvQEh1
+KWz4RB87X3GOh4HvUQOd37XTsL+AmXT7rqUIM9ieX9RExv7z7q0ehT0lDQcyko2v
+Xaj7FAS6H/UsAKK7r6SHF10hOxOP1VHfg7KnKTKgL8gf30SFDwIUKzllckM1EESv
+pqJa3iPU/wKBgB3POp5ER2w39B6Fuyr4CHN5xLp3BrnpKtcEzLxbWl1ZTe5+0lgC
+G7B+kvcxiRS4sHtMo3A26CkhSo+dEfjSsTU+rWUJpuaqQ/n+cYh3Fmxzm2HK8u/w
+K946MD8/7MUWXqG+xN1LTU/tLq2nAZ0YBgfZfyQMCLxzeqt+Y01N3fWRAoGBAJTr
+qh1l/EhdQ3dhfa+fzOwzH1lAUZKKxaSqCEmZNdheHe/Y6tItA86QRAztDPhPwbo8
+JgIBuLh+sLU77wz4whecRt4f0D6XBT9MxLRENj6b4XnpICH+nnWTYfN/fuG3SvKU
+O4d0XulxbNUHhIjHA2+wNvp07s2sWNWn8LB4ERHzAoGBALxLvb42S9x0CXvTXd+r
+RtocaT+S7He2es07o1uSSf6pnhjAhqraE+tMFv2y3AXIpKNIufs0n8ZzWhAl0v5p
+rvzgWiWsteiX+KnD4nv9I0nOiV0FLp1FSYpt3UhO55Cum58e0VG6ezqK0UVRGCQM
+gmY7Lk9jST7bctPnEPIye+WO
+-----END PRIVATE KEY-----
+',
+  ),
+);
